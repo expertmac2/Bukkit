bd05952234376e9f13a8d396b3030e35616aa907
expertmac2 <expertmac2@gmail.com>
2012-03-02 16:29:28 -0500

    Another fix (this is becoming very annoying)

--- src/test/java/org/bukkit/metadata/MetadataStoreTest.java
+++ src/test/java/org/bukkit/metadata/MetadataStoreTest.java
@@ -3,10 +3,8 @@ package org.bukkit.metadata;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 import java.util.List;
 import java.util.concurrent.Callable;
-
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.TestPlugin;
 import org.junit.Test;
